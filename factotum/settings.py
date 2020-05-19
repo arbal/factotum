d8bf7547dc4dae9f4bb6c19e3cb13ae1d3fcd92b
fang-yan-csra <30635634+fang-yan-csra@users.noreply.github.com>
2020-05-19 16:56:51 -0400

    1498: send API log to elastic index factotum_ws_logs (#1512)

--- factotum/settings.py
+++ factotum/settings.py
@@ -16,6 +16,8 @@ INTERNAL_IPS = ("127.0.0.1",)
 FILE_UPLOAD_MAX_MEMORY_SIZE = int(env.MAX_UPLOAD_SIZE)
 DATA_UPLOAD_MAX_MEMORY_SIZE = int(env.MAX_UPLOAD_SIZE)
 
+IS_WS_API = True if env.ROOT_URLCONF == "api" else False
+
 INSTALLED_APPS = [
     "dal",
     "dal_select2",
@@ -198,17 +200,27 @@ LOGGING = {
             "class": "logging.StreamHandler",
             "formatter": "django.server",
         },
+        "logstash": {
+            "level": "INFO",
+            "class": "logstash.UDPLogstashHandler",
+            "host": env.LOGSTASH_HOST,
+            "port": int(env.LOGSTASH_PORT),
+            "version": 1,
+            "message_type": "django",
+            "fqdn": False,
+            "tags": ["factotum_ws", "access"],
+        },
     },
     "loggers": {
         "django": {"handlers": ["console"], "level": "INFO", "propagate": False},
         "django.server": {
-            "handlers": ["django.server"],
+            "handlers": ["logstash", "django.server"] if IS_WS_API else ["django.server"],
             "level": "INFO",
             "propagate": False,
         },
         "gunicorn.access": {
             "level": "INFO",
-            "handlers": ["console"],
+            "handlers": ["logstash", "console"] if IS_WS_API else ["console"],
             "propagate": False,
         },
         "gunicorn.error": {
