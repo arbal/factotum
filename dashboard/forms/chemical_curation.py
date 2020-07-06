7934c17b805a01a646623e5242e7418ed6e4a54c
michael-barbour <michaelbarbour163@gmail.com>
2020-07-06 15:18:09 -0400

    Allow for blank sids chemical_curation (#1567)

--- dashboard/forms/chemical_curation.py
+++ dashboard/forms/chemical_curation.py
@@ -35,9 +35,11 @@ class ChemicalCurationForm(forms.Form):
 
     external_id = forms.ModelChoiceField(queryset=RawChem.objects.all())
     rid = RawChem._meta.get_field("rid").formfield()
-    sid = DSSToxLookup._meta.get_field("sid").formfield()
-    true_chemical_name = DSSToxLookup._meta.get_field("true_chemname").formfield()
-    true_cas = DSSToxLookup._meta.get_field("true_cas").formfield()
+    sid = DSSToxLookup._meta.get_field("sid").formfield(required=False)
+    true_chemical_name = DSSToxLookup._meta.get_field("true_chemname").formfield(
+        required=False
+    )
+    true_cas = DSSToxLookup._meta.get_field("true_cas").formfield(required=False)
 
     def __init__(self, *args, **kwargs):
         super(ChemicalCurationForm, self).__init__(*args, **kwargs)
@@ -68,31 +70,34 @@ class ChemicalCurationFormSet(DGFormSet):
         for form in self.forms:
             form.cleaned_data["true_chemname"] = form.cleaned_data["true_chemical_name"]
             dss_dict = clean_dict(form.cleaned_data, DSSToxLookup)
-            # Option 1: identical DSSToxLookup already exists; just get it
-            if DSSToxLookup.objects.filter(**dss_dict).exists():
+            # Option 1: If the sid is not set then don't attempt to match with a true chem.
+            if dss_dict.get("sid") is None:
+                dss = None
+            # Option 2: identical DSSToxLookup already exists; just get it
+            elif DSSToxLookup.objects.filter(**dss_dict).exists():
                 dss = DSSToxLookup.objects.get(**dss_dict)
             else:
-                # Option 2: matching DSSToxLookup exists in the db; get it and update its attributes
+                # Option 3: matching DSSToxLookup exists in the db; get it and update its attributes
                 if DSSToxLookup.objects.filter(sid=dss_dict["sid"]).exists():
                     dss = DSSToxLookup.objects.get(sid=dss_dict["sid"])
                     dss.true_chemname = dss_dict["true_chemname"]
                     dss.true_cas = dss_dict["true_cas"]
                     dss.updated_at = timezone.now()
                     made_dsstox.append(dss)
-                # Option 3: matching DSSToxLookup exists in new_dsstox list but not yet in db; get it
+                # Option 4: matching DSSToxLookup exists in new_dsstox list but not yet in db; get it
                 elif any(
                     dsstox for dsstox in new_dsstox if dsstox.sid == dss_dict["sid"]
                 ):
                     dss = next(
                         dsstox for dsstox in new_dsstox if dsstox.sid == dss_dict["sid"]
                     )
-                # Option 4: no matching DSSToxLookup exists; create it
+                # Option 5: no matching DSSToxLookup exists; create it
                 else:
                     dss = DSSToxLookup(**dss_dict)
                     new_dsstox.append(dss)
             chem = form.cleaned_data["external_id"]
-            chem.dsstox = dss
             chem.rid = form.cleaned_data["rid"]
+            chem.dsstox = dss
             update_chems.append(chem)
         with transaction.atomic():
             DSSToxLookup.objects.bulk_create(new_dsstox)
@@ -101,7 +106,9 @@ class ChemicalCurationFormSet(DGFormSet):
             )
             # This is a costly way to find newly created matching DSSToxLookups
             for chem in update_chems:
-                if not chem.dsstox.pk:
+                # If the dsstox model has been set but had not been saved at time of assignment
+                #  fetch the dsstox from the DB
+                if chem.dsstox and not chem.dsstox.pk:
                     chem.dsstox = DSSToxLookup.objects.get(sid=chem.dsstox.sid)
             RawChem.objects.bulk_update(update_chems, ["dsstox", "rid"])
         return len(self.forms)
