19dc3acf32e1c7958f9cbcad0c18fa2b8736b8c3
Bryan <bryanmartinezdev@gmail.com>
2020-07-21 09:05:12 -0400

    1467 limit assignment by puc kind (#1583)
    * Try to future proof dropdown
    
    * Change bulk assignment dropdown form
    
    * Scenario for non-split
    
    * Repair bulk assignment
    
    * Selenium test
    
    * remove print statement
    
    * Search - startswith()
    
    * refactor code

--- dashboard/views/puc_autocomplete.py
+++ dashboard/views/puc_autocomplete.py
@@ -1,15 +1,21 @@
 from dal import autocomplete
-from dashboard.models import PUC
+from dashboard.models import PUC, PUCKind
 from django.db.models import Q
 
 
 class PUCAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
-        qs = PUC.objects.all()
         if self.q:
-            cats = Q(gen_cat__icontains=self.q)
-            fams = Q(prod_fam__icontains=self.q)
-            types = Q(prod_type__icontains=self.q)
-            qs = qs.filter(cats | fams | types)
-
-        return qs
+            split_q = self.q.split(":")
+            if len(split_q) > 1:
+                puckind = Q(kind__name__istartswith=split_q[0])
+                puc = split_q[1]
+            else:
+                puckind = Q()
+                puc = split_q[0]
+            return PUC.objects.filter(puckind,
+                                      Q(gen_cat__icontains=puc) |
+                                      Q(prod_fam__icontains=puc) |
+                                      Q(prod_type__icontains=puc)
+                                      )
+        return None
