f476204b8ce6ebdaae81c016271fc244af258886
Bryan <bryanmartinezdev@gmail.com>
2020-07-24 10:38:30 -0400

    1536 list presence tags list page (#1587)
    * Create List page for LPTs and link from Get Data
    
    * Include "kind" in tag query
    
    * Linted with black
    
    Co-authored-by: Matt Frost <mwfrost@fastmail.fm>

--- dashboard/views/tags.py
+++ dashboard/views/tags.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import DetailView
-
+from django.shortcuts import render, get_object_or_404
 from dashboard.models import ExtractedListPresenceTag
+
+
+def list_presence_tag_list(request, template_name="tags/tag_list.html"):
+    tags = ExtractedListPresenceTag.objects.select_related("kind").all()
+    data = {}
+    data["tags"] = tags
+    return render(request, template_name, data)
 
 
 @method_decorator(login_required, name="dispatch")
