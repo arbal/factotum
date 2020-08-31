09e95656d057bd1cf97d8f0dd8d2ee2423329292
Matt Frost <mwfrost@fastmail.fm>
2020-08-31 11:23:50 -0700

    Add listener to all legend titles

--- static/js/bubble_puc_legend.js
+++ static/js/bubble_puc_legend.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
-  document.querySelectorAll(".puc-link").forEach(puc => {
-    var gen_cat = puc.getAttribute('data-gen-cat');
-    puc.style.backgroundColor = pucColors.get(gen_cat)
-  });
+$(document).ready(function () {
+    document.querySelectorAll(".puc-link").forEach(puc => {
+        var gen_cat = puc.getAttribute('data-gen-cat');
+        puc.style.backgroundColor = pucColors.get(gen_cat)
+    });
+    document.querySelectorAll(".puc-nav-title").forEach(title => {
+        title.addEventListener('click', event => {
+            var puc_id = title.getAttribute('data-puc-id');
+            console.log(puc_id)
+        })
+
+    });
 });
