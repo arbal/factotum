a3238b1eae2294bcd7013de6528987ab00901014
Matt Frost <mwfrost@fastmail.fm>
2020-09-30 07:06:50 -0700

    Corrections to PUC delivery

--- dashboard/views/visualizations.py
+++ dashboard/views/visualizations.py
@@ -14,14 +14,14 @@ class Visualizations(View):
 
     def get(self, request):
         context = {}
-        pucs = PUC.objects.with_num_products().filter(kind__code="FO").all().astree()
+        pucs = PUC.objects.filter(kind__code="FO").with_num_products().all().astree()
         for puc_name, puc_obj in pucs.items():
             puc_obj.cumnum_products = sum(
                 p.num_products for p in pucs.objects[puc_name].values()
             )
         context["formulation_pucs"] = pucs
 
-        pucs = PUC.objects.with_num_products().filter(kind__code="AR").all().astree()
+        pucs = PUC.objects.filter(kind__code="AR").with_num_products().all().astree()
         for puc_name, puc_obj in pucs.items():
             puc_obj.cumnum_products = sum(
                 p.num_products for p in pucs.objects[puc_name].values()
@@ -39,20 +39,46 @@ def bubble_PUCs(request):
         pucs = PUC.objects.dtxsid_filter(dtxsid)
     else:
         pucs = PUC.objects.all()
+
     pucs = (
         pucs.filter(kind__code=kind)
         .with_num_products()
-        .values("id", "gen_cat", "prod_fam", "prod_type", "num_products")
-        .filter(num_products__gt=0)
+        .filter(cumulative_products__gt=0)
+        .values(
+            "id",
+            "kind",
+            "gen_cat",
+            "prod_fam",
+            "prod_type",
+            "num_products",
+            "cumulative_products",
+        )
         .astree()
     )
-    # We only needed gen_cat, prod_fam, prod_type to build the tree
     for puc in pucs.values():
+        # We only needed gen_cat, prod_fam, prod_type to build the tree - now they are implicit in the structure
         puc.pop("gen_cat")
         puc.pop("prod_fam")
         puc.pop("prod_type")
-
     return JsonResponse(pucs.asdict())
+
+
+def collapsible_tree_PUCs(request):
+    """This view is used to download all of the PUCs in nested JSON form.
+    Regardless of if it is associated with an item
+    """
+    pucs = (
+        PUC.objects.all()
+        .filter(kind__code="FO")
+        .values("id", "gen_cat", "prod_fam", "prod_type")
+        .astree()
+        .asdict()
+    )
+
+    # Name the first element.  Default = Root
+    pucs["name"] = "Formulations"
+
+    return JsonResponse(pucs)
 
 
 def collapsible_tree_PUCs(request):
