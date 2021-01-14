9afb9a635f9ddab2eee4ff138a147d0bd59e769b
fang-yan-csra <30635634+fang-yan-csra@users.noreply.github.com>
2021-01-14 14:09:26 -0500

    1732: Add PUC Classification Method to Product Resource (#1762)
    * 1732: restructured product uber puc relationship to include classifcation method and other attributes
    
    * Fix tests

--- factotum/urls/api.py
+++ factotum/urls/api.py
@@ -35,6 +35,7 @@ router.register(
     apiviews.ClassificationMethodViewSet,
     base_name="classificationMethod",
 )
+router.register(r"productToPucs", apiviews.ProductToPucViewSet, basename="productToPuc")
 
 urlpatterns = [
     path("openapi.json/", docsviews.OpenAPIView.as_view(), name="openapi-schema"),
@@ -80,6 +81,16 @@ urlpatterns = [
         "products",
         view=apiviews.ProductViewSet.as_view(actions={"post": "create"}),
         name="product",
+    ),
+    path(
+        "products/<pk>/relationships/<related_field>",
+        view=apiviews.ProductRelationshipView.as_view(),
+        name="product-relationships",
+    ),
+    path(
+        "productToPucs/<pk>/relationships/<related_field>",
+        view=apiviews.ProductToPucRelationshipView.as_view(),
+        name="productToPuc-relationships",
     ),
     path("", include("django_prometheus.urls")),
 ]
