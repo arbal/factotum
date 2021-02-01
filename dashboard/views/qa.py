6a93073cf810449dc02bce869a9300167238b322
Bryan <bryanmartinezdev@gmail.com>
2021-02-01 17:28:38 -0500

    Remove unnecessary calculation from script.py (#1776)
    * Remove unnecessary calculation from script.py
    
    * Improve query performance and show Continue QA when qa_begun flag set
    
    Co-authored-by: fang-yan-csra <fang.yan@csra.com>

--- dashboard/views/qa.py
+++ dashboard/views/qa.py
@@ -39,15 +39,14 @@ def qa_extractionscript_index(request, template_name="qa/extraction_script_index
     qa_group_count = Count("extractedtext__qa_group")
     qa_complete_count = Count("extractedtext", filter=Q(extractedtext__qa_checked=True))
     percent_complete = (qa_complete_count / qa_group_count) * 100
-    texts = ExtractedText.objects.exclude(
-        data_document__data_group__group_type__code="CP"
-    )  # remove the scripts with CP texts that are associated
     extraction_scripts = (
-        Script.objects.filter(extractedtext__in=texts, script_type="EX")
+        Script.objects.filter(script_type="EX")
+        .exclude(extractedtext__data_document__data_group__group_type__code="CP")
         .exclude(title="Manual (dummy)")
         .annotate(extractedtext_count=extractedtext_count)
         .annotate(percent_complete=percent_complete)
         .annotate(qa_group_count=qa_group_count)
+        .filter(extractedtext_count__gt=0)
     )
     return render(request, template_name, {"extraction_scripts": extraction_scripts})
 
