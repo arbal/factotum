f82aaf9e0a8fa8c258cd950255308e85f91a4c32
Zach Munzenrider <33101677+zachmunzenrider@users.noreply.github.com>
2021-02-12 13:36:12 -0500

    init referer code

--- dashboard/views/data_document.py
+++ dashboard/views/data_document.py
@@ -109,6 +109,7 @@ class ChemCreateView(CreateView):
 
     def get_form_kwargs(self):
         kwargs = super(ChemCreateView, self).get_form_kwargs()
+        print(self.request.headers) #verify that this actually contains a Referer key. . . .
         if self.request.headers.get("Referer", None):
             kwargs.update({'referer': self.request.headers.get("Referer")})
         return kwargs
