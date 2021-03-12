550f130f63ac65b799f1b1079b6bf363db019cca
fang-yan-csra <30635634+fang-yan-csra@users.noreply.github.com>
2021-03-12 12:42:22 -0500

    1811: search data group tracking table by column value (#1823)
    * 1811: search data group tracking table by column value
    
    * Fix test

--- static/js/dashboard/data_group_tracking.js
+++ static/js/dashboard/data_group_tracking.js
@@ -1,6 +1,32 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    // Setup - add a text input to each header cell
+    // $('#datagroups thead tr').clone(true).appendTo('#datagroups thead');
+    $('#datagroups thead tr:eq(1) th').each(function(i) {
+        var isStep = $(this).hasClass("search-curation-step");
+        if (isStep) {
+            $("select", this).change(function() {
+                // search by whole word
+                var searchTerm = $(this).children("option:selected").val();
+                var regex = '\\b' + searchTerm + '\\b';
+                if (table.column(i).search() !== this.searchTerm) {
+                    table.column(i).search(regex, true, false).draw();
+                }
+            });
+        } else {
+            $('input', this).on('keyup change', function() {
+                if (table.column(i).search() !== this.value) {
+                    table.column(i).search(this.value).draw();
+                }
+            });
+        }
+    });
+
     var table = $('#datagroups').DataTable({
         "lengthMenu": [25, 50, 75, 100], // change number of records shown
         "order": [],
+        orderCellsTop: true,
+        "columnDefs": [
+            {"orderable": false, "targets": [-1]}
+        ]
     });
 });
