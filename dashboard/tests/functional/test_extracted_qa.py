058df42e95240b623c16457ce4b9ed090116ba22
Matt Frost <mwfrost@gmail.com>
2021-04-29 21:52:32 -0700

    Add test

--- dashboard/tests/functional/test_extracted_qa.py
+++ dashboard/tests/functional/test_extracted_qa.py
@@ -1,8 +1,16 @@
 from django.test import TestCase, tag
+from django.urls import resolve, reverse
+from lxml import html
 
 from dashboard.tests import factories
-from dashboard.tests.loader import load_model_objects
-from dashboard.models import QAGroup, ExtractedText, ExtractedFunctionalUse
+from dashboard.tests.loader import load_model_objects, fixtures_standard
+from dashboard.models import (
+    QAGroup,
+    ExtractedText,
+    ExtractedFunctionalUse,
+    Script,
+    DataGroup,
+)
 
 
 @tag("loader")
@@ -65,3 +73,52 @@ class ExtractedQaTest(TestCase):
             extracted_text=extext, qa_flag=False
         ).count()
         self.assertEqual(5, non_qa)
+
+
+@tag("loader")
+class ExtractedQaTestWithFixtures(TestCase):
+
+    fixtures = fixtures_standard
+
+    def setUp(self):
+        self.client.login(username="Karyn", password="specialP@55word")
+
+    def test_qa_manual_composition_index(self):
+        """
+        The yaml fixtures do not include any manually-extracted documents, 
+        so this test uses some factories to generate some for each Composition
+        data group
+        """
+        script = Script.objects.filter(title="Manual (dummy)", script_type="EX").first()
+        dgs = DataGroup.objects.filter(group_type__code="CO")
+        
+        for dg in dgs:
+            # add some manually-extracted records in proportion to
+            # the count of records 
+            doc_count = ExtractedText.objects.filter(
+                data_document__data_group=dg
+            ).count()
+            factory_count = (doc_count // 2) + 1
+
+            extext = factories.ExtractedTextFactory.create_batch(
+                factory_count,
+                data_document__data_group=dg,
+                extraction_script=script,
+                data_document__title=f"{dg.name} manually extracted doc",
+            )
+
+        print(ExtractedText.objects.filter(extraction_script=script).count())
+
+        response = self.client.get(reverse("qa_manual_composition_index"))
+        self.assertContains(response, "Walmart MSDS", count=6)
+
+
+        
+
+        
+
+
+        
+
+
+
