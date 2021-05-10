ae08d07a192ce63e03404292cda4767c8056fd04
Bryan <bryanmartinezdev@gmail.com>
2021-05-10 14:01:02 -0400

    download func uses button in chem detail page (#1924)
    * download func uses button in chem detail page
    
    * ran black

--- dashboard/views/chemical.py
+++ dashboard/views/chemical.py
@@ -100,6 +100,41 @@ def download_composition_chemical(request, sid):
     )
 
 
+def download_functional_uses_chemical(request, sid):
+
+    functional_uses = (
+        FunctionalUseToRawChem.objects.filter(chemical__dsstox__sid=sid)
+        .select_related(
+            "functional_use__category",
+            "chemical__extracted_text__data_document__data_group__group_type",
+        )
+        .values(
+            "chemical__extracted_text__data_document__data_group__group_type__title",
+            "chemical__extracted_text__data_document__title",
+            "chemical__extracted_text__doc_date",
+            "functional_use__report_funcuse",
+            "functional_use__category__title",
+        )
+        .order_by(
+            "chemical__extracted_text__data_document__data_group__group_type__title"
+        )
+    )
+    filename = sid + ".csv"
+    return render_to_csv_response(
+        functional_uses,
+        filename=filename,
+        append_datestamp=True,
+        use_verbose_names=False,
+        field_header_map={
+            "chemical__extracted_text__data_document__data_group__group_type__title": "Data Type",
+            "chemical__extracted_text__data_document__title": "Document",
+            "chemical__extracted_text__doc_date": "Document Date",
+            "functional_use__report_funcuse": "Reported Functional Use",
+            "functional_use__category__title": "Harmonized Functional Use",
+        },
+    )
+
+
 @login_required()
 def duplicate_chemical_records(
     request, template_name="chemicals/duplicate_chemicals.html"
