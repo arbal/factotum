b0fa60be9b8b4dce428e22e6b3021fded997e345
BryanMG957 <bryanmartinezdev@gmail.com>
2021-07-02 13:21:30 -0400

    new 'description' field

--- dashboard/models/extracted_lmrec.py
+++ dashboard/models/extracted_lmrec.py
@@ -125,6 +125,7 @@ class HarmonizedMedium(CommonInfo):
     """
 
     name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
