9f9eba358e6e90e975c1602931779541f79f7a65
Matt Frost <mwfrost@gmail.com>
2021-07-19 07:44:48 -0700

    Fix variable declaration

--- dashboard/models/news.py
+++ dashboard/models/news.py
@@ -2,13 +2,13 @@ from django.db import models
 from .common_info import CommonInfo
 from ckeditor_uploader.fields import RichTextUploadingField
 
-GETTING_STARTED_SECTION_NAME = "gettingstarted"
-
 
 class News(CommonInfo):
     subject = models.CharField(max_length=200)
     body = RichTextUploadingField()
     section = models.CharField(max_length=20, default="news")
+
+    GETTING_STARTED_SECTION_NAME = "gettingstarted"
 
     def __str__(self):
         return self.subject
