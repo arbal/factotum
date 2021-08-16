94052b434ede41b6a3f89a09cb56184a2d7c3707
Zach Munzenrider <33101677+zachmunzenrider@users.noreply.github.com>
2021-08-16 16:02:32 -0400

    migration cleanup

--- dashboard/migrations/0206_lmrec_help_text.py
+++ dashboard/migrations/0206_lmrec_help_text.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dashboard', '0205_statistical_value_updates'),
+        ('dashboard', '0205_hpdoc_foreign_key'),
     ]
 
     operations = [
