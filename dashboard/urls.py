ace562aa0982a0ac087f87099c5e08035e7529f8
Bryan <bryanmartinezdev@gmail.com>
2021-09-15 08:55:01 -0400

    2029 harmonized media detail page (#2075)
    **Contains Migration**
    
    * Harmonized Media detail page
    
    * Test documents in harmonized media
    
    * Add harmonized medium from HH records to union query
    
    * New test
    
    * linting
    
    Co-authored-by: Matt Frost <mwfrost@gmail.com>

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -348,6 +348,16 @@ urlpatterns = [
     path("fuc_d_json/", views.FUCDocumentListJson.as_view(), name="fuc_d_ajax_url"),
     path("fuc_c_json/", views.FUCChemicalListJson.as_view(), name="fuc_c_ajax_url"),
     path(
+        "hm_d_json/",
+        views.HarmonizedMediumDocumentListJson.as_view(),
+        name="hm_d_ajax_url",
+    ),
+    path(
+        "hm_c_json/",
+        views.HarmonizedMediumChemicalListJson.as_view(),
+        name="hm_c_ajax_url",
+    ),
+    path(
         "hp_json/", views.HabitsAndPracticesDocumentsJson.as_view(), name="hp_ajax_url"
     ),
     path(
@@ -422,6 +432,11 @@ urlpatterns = [
     ),
     path(
         "harmonized_media/", views.harmonized_medium_list, name="harmonized_medium_list"
+    ),
+    path(
+        "harmonized_medium/<int:pk>/",
+        views.harmonized_medium_detail,
+        name="harmonized_medium_detail",
     ),
     path(
         "dl_raw_chems_dg/<int:pk>/",
